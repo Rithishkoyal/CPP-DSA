5be1b5384f8c45b8971a2addcdaa60afccf38da2
Rithish Koyalkar <koyalkarrithish123@gmail.com>
2026-03-14 23:42:43 +0530

    Updated binary search solution

--- f.cpp
+++ f.cpp
@@ -1,16 +1,30 @@
 #include<iostream>
 using namespace std;
+int binarySearch(int arr[], int n, int x)
+{
+    int low = 0, high = n-1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x)
+            return mid;
+        else if (arr[mid] < x)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
 int main()
 {
-    int a[5];
-    cout<<"Enter the elements of the array"<<endl;
-    for(int i=0;i<5;i++)
-    {
-        cin>>a[i];
-    }
-    for(int i=0;i<5;i++)
-    {
-        cout<<a[i]<<" ";
-    }
+    int arr[] = {2, 3, 4, 10, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
+    int result = binarySearch(arr, n, x);
+    if (result == -1)
+        cout << "Element is not present in array";
+    else
+        cout << "Element is present at index " << result;
     return 0;
 }
